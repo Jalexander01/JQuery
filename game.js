4485fb939644ddabe76e1cec5eb93d8ce49b71d2
Jalexander01 <60295314+Jalexander01@users.noreply.github.com>
2021-04-08 10:34:06 -0500

    update

--- game.js
+++ game.js
@@ -1,17 +1,16 @@
-var gamePattern[];
+alert("working");
 var buttonColours = ["red", "blue", "green", "yellow"];
-   $("h1").on("mouseenter", function(){
-     $("h1").css("color", "red");
-   });
-   $("h1").on("mouseleave", function(){
-     $("h1").css("color", "#FEF2BF");
-   });
 
+var gamePattern = [];
 
-function nextsequence(){
+function nextSequence() {
+
   var randomNumber = Math.floor(Math.random() * 4);
-  return randomNumber;
- }
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
 
-var randomChosenColor = buttonColours[nextsequence()];
-gamePattern.push(randomChosenColor);
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+   var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  audio.play();
+}
